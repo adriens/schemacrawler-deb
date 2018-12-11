36c193032b170b4c61dd9448b56017044063c766
adriens <Adrien.Sales@gmail.com>
2018-12-12 09:20:54 +1100

    Add schemacrawler-shell stndalone install script

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Schemacrawler Debian package builder
 Set the version in session
 ------------------------------------------
 
-    export SCHEMACRAWLER_VERSION=15.01.05
+    export SCHEMACRAWLER_VERSION=15.03.01
 
 Download and install .deb
 ------------------------------------------
---
+++ src/main/resources/install-schemacrawler-shell.sh
@@ -0,0 +1,28 @@
+echo "###########################################################"
+echo "#                                                         #"
+echo "# Install schemacrawler Shell from scratch on your        #"
+echo "# CURRENT] current directory                              #"
+echo "#                                                         #"
+echo "###########################################################"
+
+# For now, hard code schemacrawler version
+export SC_VERSION=15.01.06
+
+
+echo "Getting schemawrawler-shell binaries ..."
+wget https://github.com/schemacrawler/SchemaCrawler/releases/download/v${SC_VERSION}/schemacrawler-${SC_VERSION}-distribution.zip
+echo "Comuting md5 :"
+md5sum schemacrawler-${SC_VERSION}-distribution.zip
+echo "Unzipping Schemacrawler Shell resources ..."
+unzip schemacrawler-${SC_VERSION}-distribution.zip
+rm schemacrawler-${SC_VERSION}-distribution.zip
+cd schemacrawler-${SC_VERSION}-distribution
+
+echo "Install shell ..."
+cd _downloader
+./download.sh shell
+
+cd ..
+cp examples/shell/schemacrawler-shell.sh ./_schemacrawler/
+cd _schemacrawler
+./schemacrawler-shell.sh
